e9a39878042da1bece889ae3a7c84d804323ef3b
SinaDeyhim <sina.deyhim@gmail.com>
2018-05-15 13:44:31 -0400

    working up to 10

--- app/src/main/java/com/example/sdeyh/androidlabs/ListItemsActivity.java
+++ app/src/main/java/com/example/sdeyh/androidlabs/ListItemsActivity.java
@@ -1,7 +1,9 @@
 package com.example.sdeyh.androidlabs;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
@@ -10,24 +12,32 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import static android.widget.ImageButton.*;
 
 public class ListItemsActivity extends Activity {
+
     protected static final String ACTIVITY_NAME = "ListItemsAvtivity";
     static final int REQUEST_IMAGE_CAPTURE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
-        Log.i(ACTIVITY_NAME,"In onCreate()");
-
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+        final Switch mySwitch = (Switch) findViewById(R.id.switch1);
+        final CheckBox myCheckBox = (CheckBox) findViewById(R.id.chBoxList);
         ImageButton button = (ImageButton) findViewById(R.id.imageButton);
-        button.setOnClickListener(new View.OnClickListener() {
 
+        //openning the camera
+        button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
@@ -36,40 +46,77 @@ public class ListItemsActivity extends Activity {
             }
         });
 
+        // showing toast for the switch
+        mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (mySwitch.isChecked()){
+                    CharSequence text = "Switch is On";//
+                    Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+                }else{
+                    CharSequence text = "Switch is Off";//
+                    Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+        myCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListItemsActivity.this);
+                // 2. Chain together various setter methods to set the dialog characteristics
+                builder.setMessage(R.string.dialog_message) //Add a dialog message to strings.xm
+                        .setTitle(R.string.dialog_title)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                finish();
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User cancelled the dialog
+                            }
+                        })
+                        .show();
+            }
+        });
+
+
+
     }
+
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
-        Log.i(ACTIVITY_NAME,"In onResume()");
+        Log.i(ACTIVITY_NAME, "In onResume()");
 
     }
 
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
-        Log.i(ACTIVITY_NAME,"In onStart()");
+        Log.i(ACTIVITY_NAME, "In onStart()");
 
     }
 
 
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
-        Log.i(ACTIVITY_NAME,"In onPause()");
+        Log.i(ACTIVITY_NAME, "In onPause()");
 
     }
 
     @Override
-    protected void onStop(){
+    protected void onStop() {
         super.onStop();
-        Log.i(ACTIVITY_NAME,"In onStop()");
+        Log.i(ACTIVITY_NAME, "In onStop()");
 
     }
 
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
-        Log.i(ACTIVITY_NAME,"In onDestroy()");
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
 
     }
 
@@ -82,6 +129,7 @@ public class ListItemsActivity extends Activity {
             imgbutn.setImageBitmap(imageBitmap);
         }
     }
+
 
 
 }
